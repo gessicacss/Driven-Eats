dfffe25993e5634b004a328b3d9d6cd9c743dbc9
Géssica Santos <santos.gessicacarolina@gmail.com>
2023-01-06 22:47:38 -0300

    adicionando função que puxa os dados do pedido escolhido e coloca no modal de confirmação de pedido

--- script.js
+++ script.js
@@ -71,3 +71,18 @@ function valor (preco) {
     valor = (Number(valor));
     return valor;
 }
+
+function fecharPedido () {
+    const modal = document.querySelector('.background');
+    modal.classList.remove('display');
+
+    const valorTotal = (precoPrato + precoBebida + precoSobremesa).toFixed(2);
+
+    document.querySelector('.nome-prato').textContent = nomePrato;
+    document.querySelector('.preco-prato').textContent = precoPrato.toFixed(2);
+    document.querySelector('.nome-bebida').textContent = nomeBebida;
+    document.querySelector('.preco-bebida').textContent = precoBebida.toFixed(2);
+    document.querySelector('.nome-sobremesa').textContent = nomeSobremesa;
+    document.querySelector('.preco-sobremesa').textContent = precoSobremesa.toFixed(2);
+    document.querySelector('.preco-total').textContent = valorTotal;
+}
